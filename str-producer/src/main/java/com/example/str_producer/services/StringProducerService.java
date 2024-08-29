e56f695ba1e8939c5ce479c607b17c4cb71c4e07
Higur1 <Higor.hungria466@gmail.com>
2024-08-29 15:02:34 -0300

    created Kafka Config and String Producer with callback function to sendMessage function

---
+++ str-producer/src/main/java/com/example/str_producer/services/StringProducerService.java
@@ -0,0 +1,31 @@
+package com.example.str_producer.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@RequiredArgsConstructor
+@Service
+public class StringProducerService {
+    @Autowired
+    private KafkaTemplate<String,String> kafkaTemplate;
+
+    public void sendMessage(String message){
+        
+        kafkaTemplate.send("str-topic", message).addCallback(
+            success -> {
+                if(success != null){
+                    log.info("Send message with sucess{}", message);
+                    log.info("Partition {}, Offset {}", 
+                        success.getRecordMetadata().partition(),
+                        success.getRecordMetadata().offset());
+                }
+            },
+            error -> log.error("Error send message")
+        );
+    }
+}
