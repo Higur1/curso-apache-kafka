d1dff44e0b59dde09acd5d2242f9ec68cec9739d
Higur1 <Higor.hungria466@gmail.com>
2024-08-30 13:51:03 -0300

    str consumer created

--- str-producer/src/main/java/com/example/str_producer/services/StringProducerService.java
+++ str-producer/src/main/java/com/example/str_producer/services/StringProducerService.java
@@ -11,21 +11,22 @@ import lombok.extern.log4j.Log4j2;
 @RequiredArgsConstructor
 @Service
 public class StringProducerService {
+
     @Autowired
     private KafkaTemplate<String,String> kafkaTemplate;
 
     public void sendMessage(String message){
-        
-        kafkaTemplate.send("str-topic", message).addCallback(
+        log.info("Send message{}", message);
+        kafkaTemplate.send("str-topic", message);/*.addCallback(
             success -> {
                 if(success != null){
                     log.info("Send message with sucess{}", message);
-                    log.info("Partition {}, Offset {}", 
+                    log.info("Partition {}, Offset {}",
                         success.getRecordMetadata().partition(),
                         success.getRecordMetadata().offset());
                 }
             },
             error -> log.error("Error send message")
-        );
+        );*/
     }
 }
