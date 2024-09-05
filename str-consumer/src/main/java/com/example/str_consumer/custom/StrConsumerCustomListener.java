fdd121c16ffd8716740684ef8b437708e63374c6
Higur1 <Higor.hungria466@gmail.com>
2024-09-05 15:56:17 -0300

    record intercept

---
+++ str-consumer/src/main/java/com/example/str_consumer/custom/StrConsumerCustomListener.java
@@ -0,0 +1,30 @@
+package com.example.str_consumer.custom;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+//iNFORMANDO PARA O COMPILADOR QUE ESSA ANOTAÇÃO TEM Q ESTAR EM TEMPO DE EXECUÇÃO
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@KafkaListener
+public @interface StrConsumerCustomListener {
+
+    /*@AliasFor(annotation = KafkaListener.class, attribute = "topicPartitions")
+    TopicPartition[] topicPartitions() default {
+            @TopicPartition(topic = "str-topic", partitions = {"0", "1"})
+    };*/
+    @AliasFor(annotation = KafkaListener.class, attribute = "topics")
+    String[] topics() default "str-topics";
+
+    @AliasFor(annotation = KafkaListener.class, attribute = "containerFactory")
+    String containerFactory() default "strContainerFactory";
+
+    @AliasFor(annotation = KafkaListener.class, attribute = "groupId")
+    String groupId() default "";
+}
